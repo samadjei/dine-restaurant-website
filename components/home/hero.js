200d36083540dbc653ef4c5fc8e9672dcd308f7b
Samuel Adjei <info.samadjei@gmail.com>
2021-04-25 17:34:09 +0100

    setup sass structure with @use and @forward

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,14 +1,29 @@
+import Image from 'next/image';
+import { Button } from '../button';
+import Link from 'next/link';
+
 const Hero = () => {
 	return (
-		<>
-			<header className="hero">
-				<h1>Exquisite dining since 1989</h1>
+		<header className="header">
+			<div className="container">
+				<div className="header__inner">
+					<nav className="logo">
+						<Image src="/logo.svg" alt="Dine logo" width={103} height={40} />
+					</nav>
+					<div className="hero">
+						<h1 className="hero--title">Exquisite dining since 1989</h1>
 
-				<p>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.</p>
+						<p className="hero--info">Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.</p>
 
-				<button>Book a table</button>
-			</header>
-		</>
+						<Link href="/signup">
+							<Button className="btn hero--btn" buttonStyle="btn--primary" buttonSize="btn--desktop">
+								Book a Table
+							</Button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</header>
 	);
 };
 
